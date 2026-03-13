ee33431e5331ab9feb72afdd72682ab6b54d28d1
Przemyslaw Wenderholm <pwenderholm@gmail.com>
2026-03-13 07:15:57 +0100

    refactor

--- js/mapa.js
+++ js/mapa.js
@@ -1,23 +1,71 @@
-var mapa; // obiekt globalny
-var dymek = new google.maps.InfoWindow();
+var mapa;
+var hotelCoordinates = [54.563935, 18.39354];
 
-function dodajMarker(opcjeMarkera)
-{
-	opcjeMarkera.map = mapa;
-	var marker = new google.maps.Marker(opcjeMarkera);
+function mapaFallback() {
+  var mapContainer = document.getElementById("mapka");
+  if (!mapContainer || mapa) {
+    return;
+  }
+
+  mapContainer.innerHTML =
+    '<iframe title="Mapa hotelu" width="100%" height="100%" frameborder="0" style="border:0" referrerpolicy="no-referrer-when-downgrade" src="https://www.openstreetmap.org/export/embed.html?bbox=18.34%2C54.52%2C18.45%2C54.60&layer=mapnik&marker=54.563935%2C18.393540" allowfullscreen></iframe>';
 }
-function mapaStart()  
-{  
-	var wspolrzedne = new google.maps.LatLng(54.563935, 18.393540);
-	var opcjeMapy = 
-	{
-		center: wspolrzedne,
-		zoom: 10,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	mapa = new google.maps.Map(document.getElementById("mapka"), opcjeMapy); 
-	
-	dymek.setContent("Tu nas znajdziesz")
-	dymek.setPosition(new google.maps.LatLng(54.563935, 18.393540));
-	dymek.open(mapa);
-};
+
+function dodajMarker(opcjeMarkera) {
+  if (!mapa || !window.L) {
+    return;
+  }
+
+  return L.marker(opcjeMarkera.coordinates || hotelCoordinates)
+    .addTo(mapa)
+    .bindPopup(opcjeMarkera.popupText || "Tu nas znajdziesz");
+}
+
+function mapaStart() {
+  if (!window.L) {
+    mapaFallback();
+    return;
+  }
+
+  var mapContainer = document.getElementById("mapka");
+  if (!mapContainer) {
+    return;
+  }
+
+  mapa = L.map("mapka", {
+    center: hotelCoordinates,
+    zoom: 12,
+    scrollWheelZoom: false,
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(mapa);
+
+  var marker = dodajMarker({
+    coordinates: hotelCoordinates,
+    popupText: "Hotel Phoenix<br>Paseo de Gomiz, 3, Alicante",
+  });
+
+  if (marker) {
+    marker.openPopup();
+  }
+}
+
+window.mapaStart = mapaStart;
+window.mapaFallback = mapaFallback;
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    if (!mapa) {
+      mapaStart();
+    }
+  }, 300);
+
+  setTimeout(function () {
+    if (!mapa) {
+      mapaFallback();
+    }
+  }, 2000);
+});
--- js/navigation-change.js
+++ js/navigation-change.js
@@ -1,14 +1,6 @@
-$(document).ready(function(){
-	  $(window).scroll(function(){
-	   var top = $(window).scrollTop();
-	   var find_class_small = $.contains('#nav', '.aaa');
-	 
-	   if(top > 180 && find_class_small == false) {
-	    $('nav').addClass('aaa');
-	   }
-	   else {
-	    $('nav').removeClass('aaa');
-	   }
-	 
-	  });
-	});
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var top = $(window).scrollTop();
+    $("nav").toggleClass("aaa", top > 180);
+  });
+});
